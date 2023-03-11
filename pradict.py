08f82fbce013141f5973d93ad7f542f908a4e164
notgoodbed <jangsuperbluesky@gmail.com>
2023-03-11 20:52:43 +0900

    hjk
    fuio

---
+++ pradict.py
@@ -0,0 +1,24 @@
+from PIL import Image
+from flask import Flask, request
+
+
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return 'No image uploaded', 400
+
+    # 이미지 파일 추출
+    image_file = request.files['image']
+    image = Image.open(image_file)
+
+    # 이미지 처리 코드 작성
+    resized_image = image.resize((224, 224))
+    # ...
+
+    return 'Image uploaded', 200
+
+if __name__ == '__main__':
+    app.run()
