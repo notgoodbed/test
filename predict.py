b1820ff2ab69c741118221616bcfda0ff223bcd6
notgoodbed <jangsuperbluesky@gmail.com>
2023-03-25 22:27:11 +0900

    awd
    awd

--- predict.py
+++ predict.py
@@ -1,12 +1,10 @@
-from PIL import Image
 from flask import Flask, request
-
+from PIL import Image
 
 app = Flask(__name__)
-print(app)
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    print(1)
     if 'image' not in request.files:
         return 'No image uploaded', 400
 
@@ -17,7 +15,7 @@ def predict():
     # 이미지 처리 코드 작성
     resized_image = image.resize((224, 224))
     # ...
-    
+
     return 'Image uploaded', 200
 
 if __name__ == '__main__':
