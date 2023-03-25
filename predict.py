f66243cd1ed65875d84cde3a7c358d49b62041cd
notgoodbed <jangsuperbluesky@gmail.com>
2023-03-25 21:09:45 +0900

    awd

--- predict.py
+++ predict.py
@@ -2,11 +2,7 @@ from PIL import Image
 from flask import Flask, request
 
 
-from flask import Flask
-from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app)
 print(app)
 @app.route('/predict', methods=['POST'])
 def predict():
